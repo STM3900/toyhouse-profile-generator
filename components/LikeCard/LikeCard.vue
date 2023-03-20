74aed3f514d836fb0c5e3a0971e3c95d9cf8792c
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-03-20 09:39:42 +0100

    add card

---
+++ components/LikeCard/LikeCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card mt-4" style="border-radius: 20px; transform: rotate(-2deg)">
+    <h5
+      class="card-header"
+      style="border-top-right-radius: 20px; border-top-left-radius: 20px"
+    >
+      <i class="fas fa-heart-broken" style="opacity: 0.5"></i>
+      Dislikes
+    </h5>
+    <div class="card-body px-3 pt-2 pb-3">
+      <div class="dislikes">
+        <ul class="list-group list-group-flush">
+          <!-- put whathever you want here -->
+          <li class="list-group-item px-0 py-1">Cras justo odio</li>
+          <li class="list-group-item px-0 py-1">Dapibus ac facilisis in</li>
+          <li class="list-group-item px-0 py-1">Vestibulum at eros</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -307,38 +307,7 @@
                   <!--
                         Dislikes card
                       -->
-                  <div
-                    class="card mt-4"
-                    style="border-radius: 20px; transform: rotate(-2deg)"
-                  >
-                    <h5
-                      class="card-header"
-                      style="
-                        border-top-right-radius: 20px;
-                        border-top-left-radius: 20px;
-                      "
-                    >
-                      <i class="fas fa-heart-broken" style="opacity: 0.5"></i>
-                      Dislikes
-                    </h5>
-                    <div class="card-body px-3 pt-2 pb-3">
-                      <div class="dislikes">
-                        <ul class="list-group list-group-flush">
-                          <!-- put whathever you want here -->
-                          <li class="list-group-item px-0 py-1">
-                            Cras justo odio
-                          </li>
-                          <li class="list-group-item px-0 py-1">
-                            Dapibus ac facilisis in
-                          </li>
-                          <li class="list-group-item px-0 py-1">
-                            Vestibulum at eros
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  
+                  <LikeCard />
                   <RelationsCard v-if="enableRelations"  @relationCardDeleted="enableRelations = false"/>
                   <button v-else @click="enableRelations = true" type="button" class="btn btn-light mt-4">Add relation card</button>
                 </div>
