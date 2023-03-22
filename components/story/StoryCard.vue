0e4d7e5d970ccba73b812eda755eed5d52ae22b0
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-03-22 01:21:14 +0100

    finish color

--- components/story/StoryCard.vue
+++ components/story/StoryCard.vue
@@ -11,21 +11,34 @@
       width: 1000px;
     "
   >
-    <div class="card-body">
-      <SuperColorPicker icon="chess-board" button-color="secondary" base-color="#ffffff" />
-      <div class="input-group">
-        <textarea
-          class="form-control"
-          aria-label="story"
-          placeholder="Write your story here c:"
-          style="min-height: 350px;"
-        ></textarea>
-        <SuperColorPicker icon="font" button-color="light" base-color="#000000" />
+      <div class="card-body" :style="{ 'background': storyBackgroundColor }" style="border-radius: 20px;">
+        <SuperColorPicker icon="chess-board" button-color="secondary" base-color="#ffffff" @color-changed="changeBackgroundColor" />
+        <div class="input-group">
+          <textarea
+            class="form-control"
+            aria-label="story"
+            placeholder="Write your story here c:"
+            style="min-height: 350px;"
+            :style="{'color': storyTextColor}"
+          ></textarea>
+          <SuperColorPicker icon="font" button-color="light" base-color="#000000" @color-changed="changeTextColor" />
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+/* dynamic color changing */
+const storyBackgroundColor = ref("#ffffff")
+const storyTextColor = ref("#000000")
+
+const changeBackgroundColor = (color) => {
+  storyBackgroundColor.value = color
+}
+
+const changeTextColor = (color) => {
+  storyTextColor.value = color
+}
+</script>
 
 <style scoped></style>
