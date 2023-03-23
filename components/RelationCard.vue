6f9a2b4734274d6af52ca7cc44e02f427aa09589
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-03-23 02:17:00 +0100

    fixes + format

--- components/RelationCard.vue
+++ components/RelationCard.vue
@@ -7,8 +7,8 @@
       alt="..."
     />
     <div class="media-body">
-      <h5 class="mt-0"><a v-if="relationInfo.link" :href="relationInfo.link" target="_blank" :style="{'color': relationInfoTitleColor}">{{ relationInfo.title }}</a><span v-else>{{ relationInfo.title }}</span> <SuperColorPicker icon="font" button-color="light" base-color="#000000" @colorChanged="changeInfoTitleColor" class="to-exclude"/></h5>
-      <span :style="{'color': relationInfoDescriptionColor}">{{ relationInfo.description }} <SuperColorPicker icon="font" button-color="light" base-color="#000000" @colorChanged="changeInfoDescriptionColor" class="to-exclude"/></span>
+      <h5 class="mt-0"><a v-if="relationInfo.link" :href="relationInfo.link" target="_blank" :style="{'color': relationInfoTitleColor}">{{ relationInfo.title }}</a><span :style="{'color': relationInfoTitleColor}" v-else>{{ relationInfo.title }}</span></h5>
+      <span :style="{'color': relationInfoDescriptionColor}">{{ relationInfo.description }}</span>
     </div>
     <button @click="$emit('deleted')" type="button" class="close to-exclude" aria-label="Close">
       <span aria-hidden="true">&times;</span>
@@ -17,20 +17,8 @@
 </template>
 
 <script setup>
-const { relationInfo } = defineProps(['relationInfo'])
+const { relationInfo } = defineProps(['relationInfo', 'relationInfoTitleColor', 'relationInfoDescriptionColor'])
 const emit = defineEmits(['deleted'])
-
-/* dynamic color changing */
-const relationInfoTitleColor = ref("#000000")
-const relationInfoDescriptionColor = ref("#000000")
-
-const changeInfoTitleColor = (color) => {
-  relationInfoTitleColor.value = color;
-}
-
-const changeInfoDescriptionColor = (color) => {
-  relationInfoDescriptionColor.value = color
-}
 </script>
 
 <style scoped></style>
--- components/RelationsCard.vue
+++ components/RelationsCard.vue
@@ -1,33 +1,30 @@
 <template>
-  <div
-    class="relations card mt-4"
-    style="border-radius: 20px; transform: rotate(2deg)"
-  >
-    <h5
-      class="card-header"
-      style="border-top-right-radius: 20px; border-top-left-radius: 20px"
-      :style="{'color': relationCardTitleColor, 'background': relationCardHeaderColor}"
-    >
+  <div class="relations card mt-4" style="border-radius: 20px; transform: rotate(2deg)">
+    <h5 class="card-header" style="border-top-right-radius: 20px; border-top-left-radius: 20px"
+      :style="{ 'color': relationCardTitleColor, 'background': relationCardHeaderColor }">
       <i class="fas fa-user-friends" style="opacity: 0.5"></i>
       Relations
-      <SuperColorPicker icon="newspaper" button-color="dark" base-color="#f7f7f7" @color-changed="changeRelationCardHeaderColor" class="to-exclude" />
-      <SuperColorPicker icon="font" button-color="light" base-color="#000000" @color-changed="changeRelationCardTitleColor" class="to-exclude" />
-      <button
-        @click="$emit('relationCardDeleted')"
-        type="button to-exclude"
-        class="close to-exclude"
-        aria-label="Close"
-      >
+      <SuperColorPicker icon="newspaper" button-color="dark" base-color="#f7f7f7"
+        @color-changed="changeRelationCardHeaderColor" class="to-exclude" />
+      <SuperColorPicker icon="font" button-color="light" base-color="#000000"
+        @color-changed="changeRelationCardTitleColor" class="to-exclude" />
+      <button @click="$emit('relationCardDeleted')" type="button to-exclude" class="close to-exclude" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </h5>
-    <div class="card-body py-20" :style="{'background': relationCardBackgroundColor}" style="border-bottom-right-radius: 20px; border-bottom-left-radius: 20px;">
-      <SuperColorPicker icon="chess-board" button-color="dark" base-color="#ffffff" @color-changed="changeRelationCardBackgroundColor" class="to-exclude"/>
-      <RelationCard
-        v-for="(relation, index) in relations"
-        :relationInfo="relation"
-        @deleted="deleteRelation(index)"
-      />
+    <div class="card-body py-20" :style="{ 'background': relationCardBackgroundColor }"
+      style="border-bottom-right-radius: 20px; border-bottom-left-radius: 20px;">
+      <SuperColorPicker icon="chess-board" button-color="dark" base-color="#ffffff"
+        @color-changed="changeRelationCardBackgroundColor" class="to-exclude" />
+      <RelationCard v-for="(relation, index) in relations" :relationInfo="relation" @deleted="deleteRelation(index)"
+        :relationInfoTitleColor="relationInfoTitleColorChar"
+        :relationInfoDescriptionColor="relationInfoDescriptionColorChar" />
+      <div class="to-exclude mb-2">
+        <SuperColorPicker icon="font" button-color="light" base-color="#000000" @colorChanged="changeInfoTitleColor"
+          class="to-exclude" />
+        <SuperColorPicker icon="font" button-color="light" base-color="#000000" @colorChanged="changeInfoDescriptionColor"
+          class="to-exclude" />
+      </div>
       <RelationCardForm class="to-exclude" @submited="addRelation" />
     </div>
   </div>
@@ -73,6 +70,18 @@ const changeRelationCardHeaderColor = (color) => {
 
 const changeRelationCardBackgroundColor = (color) => {
   relationCardBackgroundColor.value = color
+}
+
+/* dynamic color changing for element color */
+const relationInfoTitleColorChar = ref("#000000")
+const relationInfoDescriptionColorChar = ref("#000000")
+
+const changeInfoTitleColor = (color) => {
+  relationInfoTitleColorChar.value = color;
+}
+
+const changeInfoDescriptionColor = (color) => {
+  relationInfoDescriptionColorChar.value = color
 }
 </script>
 
