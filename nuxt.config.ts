e6019c7ac8d712bd7cf18d6d31ef0d5d65597e71
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-03-23 04:33:02 +0100

    seo changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'Toyhouse Profile Generator',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
 			script: [
 				{src: "https://kit.fontawesome.com/373a1c097b.js", crossorigin: "anonymous"},
 				{src: "https://code.jquery.com/jquery-3.3.1.slim.min.js", crossorigin: "anonymous", integrity: "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"},
@@ -11,6 +14,7 @@ export default defineNuxtConfig({
       link: [
 				{rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"},
 				{rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css", integrity: "sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS", crossorigin: "anonymous"},
+        {rel: "icon", type: 'image/png', href: '/favicon.png'}
 			],
     },
   },
