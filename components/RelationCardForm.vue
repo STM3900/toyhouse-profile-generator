cdfb30d90d3fcf535b2383b241724344a471e120
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-03-25 02:02:26 +0100

    add icons to inputs

--- components/RelationCardForm.vue
+++ components/RelationCardForm.vue
@@ -1,24 +1,40 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-group">
-      <label for="charName">Character name</label>
+
+    <label for="charName">Character name</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3"><i class="fas fa-user"></i></span>
+      </div>
       <input v-model="inputs.title" type="text" class="form-control" id="charName" placeholder="Name" />
     </div>
-    <div class="form-group">
-      <label for="charDescription">Character description</label>
+
+    <label for="charDescription">Character description</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3"><i class="fas fa-align-left"></i></span>
+      </div>
       <input v-model="inputs.description" type="text" class="form-control" id="charDescription"
         placeholder="Description" />
     </div>
-    <div class="form-group">
-      <label for="charLink">Character page link (optional)</label>
+
+    <label for="charLink">Character page link (optional)</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3"><i class="fas fa-link"></i></span>
+      </div>
       <input v-model="inputs.link" type="url" class="form-control" id="charLink" placeholder="Link to character page" />
     </div>
-    <div class="form-group">
-      <label for="profilePic">Character image</label>
+
+    <label for="profilePic">Character image</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3"><i class="fas fa-image"></i></span>
+      </div>
       <input v-model="inputs.img" type="text" class="form-control" id="profilePic" aria-describedby="profilePicHelp"
         placeholder="Link of profile picture" />
-      <small id="profilePicHelp" class="form-text text-muted">direct link to img is needed!</small>
     </div>
+
     <button type="submit" class="btn btn-light" :disabled="!checkForm">
       Add
     </button>
